efa6d8865e4ac124013a2bc7a212e6f8958ba1f5
aidmyst <aidmsck225@gmail.com>
2025-12-04 18:26:22 +0000

    Hubungkan css build ke app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/build/assets/app-CBtEElfD.css?v={{ time() }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
